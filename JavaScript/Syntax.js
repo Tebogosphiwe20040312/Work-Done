6c55509d0e60226cb0237b14b86bd6b09ba84c4e
Tebogo Sphiwe Jiyane <tebogosiphiwejane@gmail.com>
2025-08-03 17:29:50 +0200

    Add files via upload

---
+++ JavaScript/Syntax.js
@@ -0,0 +1,8 @@
+//How to declare variables
+var x;
+let y;
+
+//How to use variables
+x = 5;
+y = 10;
+let z = x + y;
---
+++ JavaScript/myScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+function displayImage() {
+    document.getElementById("image").src = "pagani.jpg";
+    document.getElementById("image").style.width="80%";
+}
